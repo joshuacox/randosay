e4e19338bfbbe0fcaabe61c6007c9cc01ef537cd
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-12-12 11:04:14 -0600

    bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # randosay
 
 This is a small utility to wrap fortunes with a horizontal rule, and then to randomly select cowsay, cowthink, ponysay, ponythink, lolcat, or plain and then finish off with another horizontal rule.
+
+```
+curl -sL https://raw.githubusercontent.com/joshuacox/randosay/refs/heads/main/bootstrap.sh | bash
+```
---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+THIS_NAME=randosay
+THIS_GH=joshuacox
+THIS_BRANCH=main
+TMP_DIR=$(mktemp -d --suffix='${THIS_NAME}')
+cleanup_func () {
+  echo rm -Rf ${TMP}
+}
+trap cleanup_func EXIT
+
+cd $TMP_DIR
+curl -L -o ${THIS_NAME}-${THIS_BRANCH}.zip https://github.com/${THIS_GH}/${THIS_NAME}/archive/refs/heads/${THIS_BRANCH}.zip
+unzip ${THIS_NAME}-${THIS_BRANCH}.zip
+cd ${THIS_NAME}-${THIS_BRANCH}
+cmake .
+make
+sudo make install
+cd
