ebdc5378fca84f217d3d394a3312922f54d8aa7d
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-12-12 11:09:00 -0600

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 THIS_NAME=randosay
 THIS_GH=joshuacox
 THIS_BRANCH=main
-TMP_DIR=$(mktemp -d --suffix='${THIS_NAME}')
+TMP_DIR=$(mktemp -d --suffix="${THIS_NAME}")
 cleanup_func () {
   echo rm -Rf ${TMP}
 }
